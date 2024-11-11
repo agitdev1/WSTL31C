bcf628739a3eef7b60e798e80b0a1208d9ec8f51
Agather <agather.medina@gmail.com>
2024-11-11 20:53:15 +0800

    inidit ang h2 sa index.php

--- pages/index.php
+++ pages/index.php
@@ -22,7 +22,9 @@ include '../components/navbar.php';
         <hr class="full-width-hr">
         <div class="content-section right-image">
             <div class="content-text">
-                <h1 style="color:#0887B2; text-align: start;  margin-right: 20px ; "> SUSTAINABLE DEVELOPMENT GOALS 17 </h1>
+                <h1 style="color:#0887B2; text-align: start; margin-right: 20px;">
+                    <span>SUSTAINABLE DEVELOPMENT 
+                    <br>GOALS 17 </h1>
                 <p style ="text-align: start; margin-right: 40px  ; " >Discover and engage in meaningful volunteering opportunities that align with SDG 17, promoting collaboration for sustainable development. Join a community dedicated to fostering partnerships that drive global progress and create lasting, positive impacts within local communities.</p>
             </div>
             <img src="../assets/images/sdg17_2.webp" alt="Sustainable Development Goals" class="content-image">
