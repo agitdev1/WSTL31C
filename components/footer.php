ac0a542aa17013e4351fde3f8ed3521c1ed75f0a
Agather <agather.medina@gmail.com>
2024-11-11 14:40:37 +0800

    first commit

---
+++ components/footer.php
@@ -0,0 +1,15 @@
+<!-- footer.php -->
+ <footer>
+<div class="footer">
+    <div class="footer-content">
+        <div class="footer-links">
+            <a href="about.php">About Us</a>
+            <a href="privacy.php">Privacy Policy</a>
+            <a href="contact.php">Contact Us</a>
+        </div>
+        <div class="footer-copyright">
+            &copy; <?php echo date("Y"); ?> Voluntech. All rights reserved.
+        </div>
+    </div>
+</div>
+</footer>
---
+++ pages/index.php
@@ -0,0 +1,34 @@
+<?php
+include '../components/navbar.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Voluntech</title>
+    <link rel="stylesheet" href="../assets/css/styles.css">
+</head>
+<body>
+    <div class="main-content">
+        <h1>Empowering Change 
+        <br> Through <span style="color:#DD4040;">Volunteering</span></h1>
+        <div class="content-section">
+            <img src="../assets/images/banner2.svg" alt="Volunteering Banner" class="content-image">
+            <p>We aim to empower students, faculty, and staff of Lyceum of the Philippines University - Manila to seamlessly connect and contribute to meaningful volunteering opportunities within the LPU community and the wider Philippine society.</p>
+        </div>
+        <hr class="full-width-hr">
+        <div class="content-section">
+        <p>Discover and engage in volunteering opportunities that support SDG 17, fostering partnerships for global progress and community impact.</p>
+            <img src="../assets/images/sdg17.png" alt="Sustainable Development Goals" class="content-image">
+        </div>
+    </div>
+    <div class="buttons">
+        <a href="/register.php" class="gradient-button">Register</a>
+        <a href="/login.php" class="gradient-button">Login</a>
+    </div>
+    <br>
+    <?php include '../components/footer.php'; ?>
+</body>
+</html>
+
