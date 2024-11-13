507dbb5e27f43db131961ee1e1eb21eb0de0a342
Matt <128983798+Xenomatte@users.noreply.github.com>
2024-11-13 22:35:00 +0800

    Added forgot password and password reset
    Backend for both files needs more depth, testing phase for now...

--- pages/reset-password.php
+++ pages/reset-password.php
@@ -9,7 +9,7 @@ include '../components/navbar.php';
     <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
     <meta http-equiv="Pragma" content="no-cache">
     <meta http-equiv="Expires" content="0">
-    <title>Forgot Password</title>
+    <title>Reset Password</title>
     <link rel="stylesheet" href="../assets/css/styles.css?v=<?php echo time(); ?>">
     <link rel="stylesheet" href="../assets/css/forgot-password.css?v=<?php echo time(); ?>">
 </head>
@@ -17,10 +17,11 @@ include '../components/navbar.php';
     <div class="page-container">
         <div class="content-wrap">
             <div class="form-container">
-                <form id="forgot-password-form" action="login.php" method="post" class="form">
-                    <h1 class="form-h1">Forgot your password?</h1>
-                    <input type="text" id="email" name="email" placeholder="Enter your email" required>
-                    <button type="submit">Send Link</button>
+                <form id="reset-password-form" action="login.php" method="post" class="form">
+                    <h1 class="form-h1">Rest your password</h1>
+                    <input type="password" id="new-password" name="new-password" placeholder="Enter your new password" required>
+                    <input type="password" id="confirm-new-password" name="confirm-new-password" placeholder="Confirm your new password" required>
+                    <button type="submit">Reset Password</button>
                     <div id="error-message" class="error-message"></div>
                     <div id="success-message" class="success-message"></div>
                 </form>
@@ -29,7 +30,7 @@ include '../components/navbar.php';
         </div>
         <?php include '../components/footer.php'; ?>
     </div>
-    <script src="../assets/js/forgot-password.js"></script>
+    <script src="../assets/js/reset-password.js"></script>
 </body>
 </html>
 
