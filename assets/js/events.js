788c91624facba066d47e9a87c7f70c330073021
Aga <165638077+agitdev1@users.noreply.github.com>
2024-12-20 18:03:40 +0800

    Delete assets directory

--- assets/js/events.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('form').addEventListener('submit', function(e) {
-    const button = this.querySelector('.donate-button');
-    button.classList.add('loading');
-    button.disabled = true;
-});
