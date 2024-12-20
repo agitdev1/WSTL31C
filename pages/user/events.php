20b328649522e3e18ec98f7b96193ba3262df810
Agather <agather.medina@lpunetwork.edu.ph>
2024-12-20 16:28:30 +0800

    Update events.php

--- pages/user/events.php
+++ pages/user/events.php
@@ -5,56 +5,55 @@ $events = [
     [
         'image' => '../assets/images/event1.jpg',
         'title' => 'Event Volunteers Needed for Feline Frolic Event',
-        'location' => 'White Sand Beach, Coastal City',
+        "link" => "../pages/vol-event.php",
+        'location' => 'White Sand Beach, Coastal City, 56789',
         'date' => 'Dec 20, 2024',
         'time' => '07:00 AM - 11:00 AM',
-        'organization' => 'Animal Protectors',
-        'redirect' => 'vol-event.php'
+        'organization' => 'Animal Protectors'
     ],
     [
         'image' => '../assets/images/event2.jpg',
         'title' => 'Luntiang Marikina: Marikina River Clean Up Activity for November 2024',
-        'location' => '123 Charity Street, Green City',
+        "link" => "../pages/vol-event.php",
+        'location' => '123 Charity Street, Green City, 12345',
         'date' => 'Jan 10, 2025',
         'time' => '09:00 AM - 01:00 PM',
-        'organization' => 'Green Earth Volunteers',
-        'redirect' => 'vol-event.php'
+        'organization' => 'Green Earth Volunteers'
     ],
     [
         'image' => '../assets/images/event3.png',
         'title' => 'Sacks of Rice for Karinderia ni Mang Urot',
-        'location' => '453 Giving Street, Global City',
+        'location' => '453 Giving Street, Global City, 12345',
         'date' => 'Jan 12, 2025',
-        'time' => 'N/A',
-        'organization' => 'The Givers',
-        'redirect' => 'vol-donate.php'
+        'time' => '07:00 AM - 05:00 PM',
+        'organization' => 'The Givers'
     ],
     [
         "image" => "../assets/images/events/event1.png",
         "title" => "Project Baon, A Day of Volunteer Service",
-        "location" => "789 Charity Lane, Hope Town",
+        "location" => "789 Charity Lane, Hope Town, 54321",
         "date" => "Mar 15, 2025",
         "time" => "10:00 AM - 02:00 PM",
         "organization" => "Helping Hands",
-        "redirect" => 'vol-event.php'
+        "event_name" => "Project Baon, A Day of Volunteer Service"
     ],
     [
         "image" => "../assets/images/events/event2.png",
         "title" => "Feed the Hungry",
-        "location" => "321 Green Park, Nature City",
+        "location" => "321 Green Park, Nature City, 98765",
         "date" => "Apr 22, 2025",
         "time" => "07:00 AM - 11:00 AM",
         "organization" => "Eco Warriors",
-        "redirect" => 'vol-event.php'
+        "event_name" => "Tree Planting Volunteer Day"
     ],
     [
         "image" => "../assets/images/events/event3.png",
         "title" => "Blood Donation Volunteer Support",
-        "location" => "654 Health Blvd, Wellness City",
+        "location" => "654 Health Blvd, Wellness City, 11223",
         "date" => "May 5, 2025",
         "time" => "09:00 AM - 03:00 PM",
         "organization" => "Life Savers",
-        "redirect" => 'vol-donate.php'
+        "event_name" => "Blood Donation Volunteer Support"
     ]
 ];
 ?>
@@ -74,8 +73,9 @@ $events = [
     <h1>Upcoming Events</h1>
     <div class="gallery">
         <?php foreach ($events as $event): ?>
-            <div class="gallery-item" data-redirect="<?php echo htmlspecialchars($event['redirect']); ?>">
-                <img src="<?php echo htmlspecialchars($event['image']); ?>" alt="<?php echo htmlspecialchars($event['title']); ?>">
+            <div class="gallery-item">
+                    <img src="<?php echo htmlspecialchars($event['image']); ?>" alt="<?php echo htmlspecialchars($event['title']); ?>">
+                </a>
                 <div class="desc"><?php echo htmlspecialchars($event['title']); ?></div>
                 <p class="card-location">
                     <i class="fa fa-location-arrow" aria-hidden="true"></i>
@@ -102,12 +102,14 @@ $events = [
                     <input type="hidden" name="date" value="<?php echo htmlspecialchars($event['date']); ?>">
                     <input type="hidden" name="time" value="<?php echo htmlspecialchars($event['time']); ?>">
                     <input type="hidden" name="organization" value="<?php echo htmlspecialchars($event['organization']); ?>">
+                    <button type="submit" class="donate-button">Register Now</button>
                 </form>
             </div>
         <?php endforeach; ?>
     </div>
 </div>
 <?php require_once '../components/footer2.php'; ?>
+<<<<<<< HEAD:pages/user/events.php
 <script>
     document.addEventListener('DOMContentLoaded', function() {
         const galleryItems = document.querySelectorAll('.gallery-item');
@@ -120,5 +122,8 @@ $events = [
             });
         });
     });
+=======
+<script src="../assets/js/events.js"></script>
+>>>>>>> parent of 931b028 (Gyawa):pages/events.php
 </body>
 </html>
